47afec40ca79851946910cb5f1826a06e30cb1f8
Julien REPPLINGER <jul.repp@gmail.com>
2024-02-29 19:02:58 +0100

    remove unused imports

--- app/src/main/java/fr/isen/repplinger/androiderestaurant/activity/CategoryActivity.kt
+++ app/src/main/java/fr/isen/repplinger/androiderestaurant/activity/CategoryActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Divider
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,7 +23,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
